76f8c8e359ffb275b9576e1848986acfd78f478c
FrancineMoraes <francinemoraes08@hotmail.com>
2018-09-24 20:59:30 -0300

    ajustando rota

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'SiteController@index');
+Route::get('/', 'GaleriaController@index');
 
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware' => 'auth'], function() {
    
